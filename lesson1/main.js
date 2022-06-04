81bf4ae907896703b5a29c23762d6d3ac638ea68
MaksimBelke <106869590+MaksimBelke@users.noreply.github.com>
2022-06-04 21:42:16 +0300

    lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,22 @@
+// const a = "a"
+//
+//
+// let c = "c"
+// const bool = true
+// const bool2 =false
+// const obj = {
+//     key: "value",
+//     username: "aybek",
+//     data: {
+//         first_name: "jack",
+//         last_name: "rus"
+//     };
+//
+// const arr = ["Aidana", true, false, 34];
+
+
+// const name = promt(message: "Как тебя зовут")
+
+// const age = alert()
+// const arr3 = [[[[[[[[[[[[[[[5]]]]]]]]]]]]]],
+// console.log(arr3[0])
